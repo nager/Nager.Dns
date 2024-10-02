9571bd42d6271d36b3649e8290dd216d3383d2ac
Tino Hager <tino.hager@nager.at>
2024-10-02 20:49:58 +0200

    Update DnsAnswerType.cs

--- src/Nager.Dns/Models/DnsAnswerType.cs
+++ src/Nager.Dns/Models/DnsAnswerType.cs
@@ -1,16 +1,58 @@
 ﻿namespace Nager.Dns.Models
 {
+    /// <summary>
+    /// Dns Answer Type
+    /// </summary>
     public enum DnsAnswerType
     {
-        A = 1,      // Standard DNS RR type
-        NS = 2,     // Name server
-        CNAME = 5,  // Canonical name for an alias
-        SOA = 6,    // Start of authority for a zone
-        PTR = 12,   // Domain name pointer
-        MX = 15,    // Mail exchange
-        TXT = 16,   // Text strings
-        AAAA = 28,  // IPv6 address
-        SRV = 33,   // Service locator
-        ANY = 255   // Wildcard match
+        /// <summary>
+        /// Address record
+        /// </summary>
+        A = 1,
+
+        /// <summary>
+        /// Name server record
+        /// </summary>
+        NS = 2,
+
+        /// <summary>
+        /// Canonical name record
+        /// </summary>
+        CNAME = 5,
+
+        /// <summary>
+        /// Start of authority record
+        /// </summary>
+        SOA = 6,
+
+        /// <summary>
+        /// PTR Resource Record
+        /// </summary>
+        PTR = 12,
+
+        /// <summary>
+        /// Mail exchange record
+        /// </summary>
+        MX = 15,
+
+        /// <summary>
+        /// 	Text record
+        /// </summary>
+        TXT = 16,
+
+        /// <summary>
+        /// IPv6 address record
+        /// </summary>
+        AAAA = 28,
+
+        /// <summary>
+        /// Service locator
+        /// </summary>
+        SRV = 33,
+
+        /// <summary>
+        /// All cached records
+        /// </summary>
+        ANY = 255
     }
 }
